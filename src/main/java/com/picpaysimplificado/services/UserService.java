5fa3fb3d099d5ecbd246a0c77f07a9e0ba9463b3
Bruno H. Homem <brunohhomem@gmail.com>
2023-12-30 16:42:03 -0300

    feat: insert and find user ok

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -5,7 +5,6 @@ import com.picpaysimplificado.domain.user.UserType;
 import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
